6a9096b6ba79690dfdd19acb649f3cc1a0fe0c70
itsachaljain <achaljain785@gmail.com>
2020-09-13 20:55:34 +0530

    Passport

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 var session = require("express-session");
 var FileStore = require("session-file-store")(session);
+var passport = require("passport");
+var authenticate = require("./authenticate");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
@@ -15,7 +17,7 @@ var leaderRouter = require("./routes/leaderRouter");
 const mongoose = require("mongoose");
 
 const Dishes = require("./models/dishes");
-const { signedCookie } = require("cookie-parser");
+const { initialize } = require("passport");
 
 const url = "mongodb://localhost:27017/conFusion";
 const connect = mongoose.connect(url);
@@ -48,25 +50,19 @@ app.use(
 		store: new FileStore(),
 	})
 );
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 
 function auth(req, res, next) {
-	console.log(req.session);
-
-	if (!req.session.user) {
+	if (!req.user) {
 		var err = new Error("You are not authenticated!");
 		err.status = 403;
 		return next(err);
 	} else {
-		if (req.session.user === "authenticated") {
-			next();
-		} else {
-			var err = new Error("You are not authenticated!");
-			err.status = 403;
-			return next(err);
-		}
+		next();
 	}
 }
 
