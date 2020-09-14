b42337e18453bd1167e0ef5b16dc0857976cf719
itsachaljain <achaljain785@gmail.com>
2020-09-14 19:25:04 +0530

    Passport JWT

--- routes/promoRouter.js
+++ routes/promoRouter.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const authenticate = require("../authenticate");
 
 const Promotions = require("../models/promotions");
 
@@ -22,7 +23,7 @@ promoRouter
 			)
 			.catch((err) => next(err));
 	})
-	.post((req, res, next) => {
+	.post(authenticate.verifyUser, (req, res, next) => {
 		Promotions.create(req.body)
 			.then(
 				(promotion) => {
@@ -35,11 +36,11 @@ promoRouter
 			)
 			.catch((err) => next(err));
 	})
-	.put((req, res, next) => {
+	.put(authenticate.verifyUser, (req, res, next) => {
 		res.statusCode = 403;
 		res.end("PUT operation not supported on /promotions");
 	})
-	.delete((req, res, next) => {
+	.delete(authenticate.verifyUser, (req, res, next) => {
 		Promotions.remove({})
 			.then(
 				(resp) => {
@@ -66,13 +67,13 @@ promoRouter
 			)
 			.catch((err) => next(err));
 	})
-	.post((req, res, next) => {
+	.post(authenticate.verifyUser, (req, res, next) => {
 		res.statusCode = 403;
 		res.end(
 			"POST operation not supported on /promotions/" + req.params.promotionId
 		);
 	})
-	.put((req, res, next) => {
+	.put(authenticate.verifyUser, (req, res, next) => {
 		Promotions.findByIdAndUpdate(
 			req.params.promotionId,
 			{
@@ -90,7 +91,7 @@ promoRouter
 			)
 			.catch((err) => next(err));
 	})
-	.delete((req, res, next) => {
+	.delete(authenticate.verifyUser, (req, res, next) => {
 		Promotions.findByIdAndRemove(req.params.promotionId)
 			.then(
 				(resp) => {
