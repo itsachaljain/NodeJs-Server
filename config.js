ad33d6d84e9cc5d34c0b6699897fcc9dff18d872
itsachaljain <achaljain785@gmail.com>
2020-09-16 02:19:52 +0530

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
 	secretKey: "12345-67890-09876-54321",
 	mongoUrl: "mongodb://localhost:27017/conFusion",
+	facebook: {
+		clientId: "940826506414117",
+		clientSecret: "e9db1809edc2838d04936d3d85eadad1",
+	},
 };
